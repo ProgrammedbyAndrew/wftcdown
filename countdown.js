eb534dc526b176d3fabc0f6238adb6c069ea6d97
Andrew <44824558+ProgrammedbyAndrew@users.noreply.github.com>
2023-09-20 10:45:53 -0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -19,32 +19,18 @@ function isCountingTime(now) {
     }
 }
 
-function getTotalActiveHours() {
-    let activeHours = 0;
-    for (let i = 0; i < (endDate - startDate) / 3600000; i++) { // loop by hour
-        const tempDate = new Date(startDate.getTime() + i * 3600000);
+function getTotalActiveSeconds() {
+    let activeSeconds = 0;
+    for (let i = 0; i < (endDate - startDate) / 1000; i++) { // loop by second
+        const tempDate = new Date(startDate.getTime() + i * 1000);
         if (isCountingTime(tempDate)) {
-            activeHours++;
+            activeSeconds++;
         }
     }
-    return activeHours;
+    return activeSeconds;
 }
 
-const baseRate = totalCustomers / (getTotalActiveHours() * 3600); // customers per second
-
-function getDynamicRate(now) {
-    const hour = now.getHours();
-    const dayOfWeek = now.getDay();
-
-    // Peak times on weekend nights
-    if ((dayOfWeek === 6 || dayOfWeek === 0) && (hour >= 18 && hour <= 23)) {
-        return baseRate * 2; // double the base rate
-    }
-    // Randomize rate for other times
-    else {
-        return baseRate * (1 + Math.random()); // Random rate between 100% to 200% of the base rate
-    }
-}
+const exactRate = totalCustomers / getTotalActiveSeconds(); // customers per second
 
 function calculateCurrentCount() {
     const now = new Date();
@@ -53,7 +39,7 @@ function calculateCurrentCount() {
     for (let i = 0; i < elapsedSeconds; i++) {
         const tempDate = new Date(startDate.getTime() + i * 1000);
         if (isCountingTime(tempDate)) {
-            currentCustomerCount += getDynamicRate(tempDate);
+            currentCustomerCount += exactRate;
         }
     }
 
@@ -65,13 +51,27 @@ function calculateCurrentCount() {
 }
 
 // Initial update
-document.getElementById('customerCount').textContent = calculateCurrentCount().toLocaleString();
+document.getElementById('customerCount').textContent = calculateCurrentCount();
 
 // Update every second
 setInterval(() => {
     const now = new Date();
     if (isCountingTime(now)) {
-        currentCustomerCount += getDynamicRate(now);
-        document.getElementById('customerCount').textContent = Math.floor(currentCustomerCount).toLocaleString();
+        currentCustomerCount += exactRate;
+        document.getElementById('customerCount').textContent = Math.floor(currentCustomerCount);
     }
 }, 1000);
+
+// Simulation (optional, for verification purposes)
+function simulateToEnd() {
+    let simulationCount = startingCount;
+    for (let i = 0; i < (endDate - startDate) / 1000; i++) {
+        const tempDate = new Date(startDate.getTime() + i * 1000);
+        if (isCountingTime(tempDate)) {
+            simulationCount += exactRate;
+        }
+    }
+    return simulationCount;
+}
+
+console.log("Simulation result:", simulateToEnd());
